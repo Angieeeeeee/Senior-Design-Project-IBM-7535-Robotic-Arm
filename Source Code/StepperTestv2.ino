8bd70f28de81d7c465a11efc01a547bcaf7d7a11
Darkstar88x <107122632+Darkstar88x@users.noreply.github.com>
2025-08-06 15:25:50 -0500

    Will operate stepper motor and stop.

--- Source Code/StepperTestv2.ino
+++ Source Code/StepperTestv2.ino
@@ -1,5 +1,7 @@
 #include "OptaBlue.h"
 
+/*Test will spin stepper clockwise and stop. There is a metal belt clamp on the stepper this should be position so that it does not hit gears! OA_PWM_CH_1 would spin the stepper in the other direction*/
+
 void setup() {
   OptaController.begin();
 
@@ -8,16 +10,18 @@ void setup() {
     delay(500);
   }
 
-  // Force the correct type
-  //Opta::AnalogExpansion aexp = (Opta::AnalogExpansion&)OptaController.getExpansion(0);
+  Opta::AnalogExpansion aexp = (Opta::AnalogExpansion&)OptaController.getExpansion(0);
 
-  // Set PWM output on PWM1+ (P1)
-  //aexp.setPwm(OA_PWM_CH_0, 50, 20);  // 100% duty cycle = constant high
+  // Spin for 0.25 seconds
+  aexp.setPwm(OA_PWM_CH_0, 1600, 25);  // 1600 Hz, 25% duty
+  delay(400);
+
+  // Stop motor
+  aexp.setPwm(OA_PWM_CH_0, 1600, 0);  // 0% duty (off)
 }
 
 void loop() {
-  OptaController.update();
-  Opta::AnalogExpansion aexp = (Opta::AnalogExpansion&)OptaController.getExpansion(0);
-  aexp.setPwm(OA_PWM_CH_0, 400, 25);
-  delay(100);
+  // Do nothing — motor has stopped
+  while (1)
+    ;  // Prevent further execution
 }
